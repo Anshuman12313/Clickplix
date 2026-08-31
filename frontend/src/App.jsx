967896defc2038f05681ce36b51a200dae64d27b
Anshuman12313 <anshumansalempur@gmail.com>
2026-08-31 19:32:05 +0530

    Agent Host changes for agents/frontend-upgrade-mobile-preview

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,29 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
 
 import Home from "./pages/Home";
-import Register from "./pages/Register"
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import CreateGroup from "./pages/CreateGroup"; 
+import CreateGroup from "./pages/CreateGroup";
 import Group from "./pages/Group";
-import Camera from "./pages/Camera"
-import Login from "./pages/Login"
-function App(){
-    return(
-        <BrowserRouter>
-        <Routes>
-            <Route
-            path="/"
-            element={<Home/>}
-            />
-            <Route
-            path="/register"
-            element={<Register/>}
-            />
-            <Route
-            path="/dashboard"
-            element={<Dashboard/>}
-            />
-            <Route
-            path="/groups/create"
-            element={<CreateGroup/>}
-                />
+import Camera from "./pages/Camera";
+import Login from "./pages/Login";
 
-                <Route
-                path="/groups/:groupId"
-                element={<Group/>}
-                />
-                <Route
-                path="/camera/:groupId"
-                element={<Camera/>}
-                />
-                <Route
-                path="/login"
-                element={<Login/>}
-                />
-        </Routes>
-
-        </BrowserRouter>
-    );
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/groups/create" element={<CreateGroup />} />
+        <Route path="/groups/:groupId" element={<Group />} />
+        <Route path="/camera/:groupId" element={<Camera />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
